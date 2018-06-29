320b7a7eee3d2142f9c94020af800b0d8672c0d5
Rahul <rahul91341@gmail.com>
2018-06-29 23:37:28 +0530

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+
+module.exports = {
+  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: 'bundle.js',  // Name of generated bundle after build
+    publicPath: '/' // public URL of the output directory when referenced in a browser
+  },
+  module: {  // where we defined file patterns and their loaders
+      rules: [ 
+          {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: [
+              /node_modules/
+            ]
+          },
+          {
+            test: /\.(scss)$/,
+            use: [{
+              loader: 'style-loader', // inject CSS to page
+            }, {
+              loader: 'css-loader', // translates CSS into CommonJS modules
+            }, {
+              loader: 'postcss-loader', // Run post css actions
+              options: {
+                plugins: function () { // post css plugins, can be exported to postcss.config.js
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            }, {
+              loader: 'sass-loader' // compiles Sass to CSS
+            }]
+          },
+      ]
+  },
+  plugins: [  // Array of plugins to apply to build chunk
+      new HtmlWebpackPlugin({
+          template: __dirname + "/src/public/index.html",
+          inject: 'body'
+      })
+  ],
+  devServer: {  // configuration for webpack-dev-server
+      contentBase: './src/public',  //source of static assets
+      port: 7700, // port to run dev-server
+  } 
+};
